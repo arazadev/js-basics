9e4c62962f4b1187538328c160b5f30626829e47
“arazadev” <263957307+arazadev@users.noreply.github.com>
2026-06-24 15:09:19 +0500

    OOP

---
+++ Advance/OOP/oop.js
@@ -0,0 +1,46 @@
+// Object literals
+const user = {
+  username: "Ali",
+  loginCount: 3,
+  signedIn: true,
+  getUserDetails: function () {
+    console.log("User Found");
+    console.log(`UserName: ${this.username}`)
+    console.log(this);// -> current context
+  },
+  // The this keyword behaves differently in regular functions compared to arrow functions.we get undefined when use the arrow function
+
+  getUserDetails: () => console.log(`UserName: ${this.username}`)
+};
+console.log(user["getUserDetails"]()); // access through square brackets
+
+console.log(user.getUserDetails());
+
+console.log(this); // -> gives the empty object and can changed. In browser it gives the window object
+
+// new keyword is the constructor function
+const promiseOne = new Promise()
+const date = new Date()
+
+function user1(username, loginCount, IsLoggedIn) {
+  // left one is the variable
+  this.username = username;
+  this.loginCount = loginCount;
+  this.IsLoggedIn = IsLoggedIn;
+  // not need to return but best to write
+  return this;
+}
+const a = user1("abc", 23, true);
+console.log(a);
+
+// Use 'new' to create an instance of the user1 object
+const userOne = new user1("js", 12, false);
+const userTwo = new user1("Ali", 13, true);
+console.log(userOne); // Output: user1 { username: 'js', loginCount: 12, IsLoggedIn: false }
+
+ console.log(userTwo.constructor);
+
+console.log(userOne instanceof user1); // true
+console.log(userTwo instanceof user1); // true
+
+console.log(userOne.constructor === userTwo.constructor)
